77ac9b74565dde8e5c7018680e3389786df093a8
Karl <karl@corahtech.io>
2023-07-17 21:50:29 +1000

    updates to current scripts plus data generation

--- data/random.js
+++ data/random.js
@@ -5,7 +5,6 @@ function numberRange(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random()*(max-min + 1)) + min;
-
 }
 
 function dateInRange(){
@@ -14,3 +13,6 @@ function dateInRange(){
 
 console.log(numberRange(1000, 100000000000));
 console.log(dateInRange())
+
+export{numberRange}
+export{dateInRange}
